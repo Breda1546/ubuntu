1cada40ecbf07fb48a08aece707c021c320ed302
Breda1546 <44686120+Breda1546@users.noreply.github.com>
2018-11-01 20:10:11 -0300

    Create teste.sh

---
+++ teste.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Meu Primeiro Script
+
+#Listando os diretorios do linux
+ls -lha /
+read
+clear
+
+#Atualizando o Apt ou Apt-Get
+apt update
+echo "Listas atualizadas!!!"
+read
+cls
+
+#Desligando o sevidor
+shutdown -h now 
